310dcd96087e740837c6f39c65d458b594f7098f
Murfew <matthew.hurley953@gmail.com>
2026-09-04 15:13:49 -0400

    improve styling of sidebar sign out button

--- components/dashboard-sign-out.tsx
+++ components/dashboard-sign-out.tsx
@@ -1,25 +1,41 @@
 "use client";
 
+import type { ErrorContext } from "better-auth/client";
 import { LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { toast } from "sonner";
 import { SidebarMenuButton } from "@/components/ui/sidebar";
+import { Spinner } from "@/components/ui/spinner";
 import { authClient } from "@/lib/auth-client";
 
 export default function DashboardSignOut() {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handleSignOut = async () =>
     await authClient.signOut({
       fetchOptions: {
+        onRequest: () => {
+          setLoading(true);
+        },
         onSuccess: () => {
           router.push("/sign-in");
+        },
+        onError: (ctx: ErrorContext) => {
+          setLoading(false);
+          toast.error(ctx.error.message);
         },
       },
     });
 
   return (
-    <SidebarMenuButton tooltip="Sign out" onClick={handleSignOut}>
-      <LogOutIcon />
+    <SidebarMenuButton
+      tooltip="Sign out"
+      onClick={handleSignOut}
+      disabled={loading}
+    >
+      {loading ? <Spinner /> : <LogOutIcon />}
       <span>Sign out</span>
     </SidebarMenuButton>
   );

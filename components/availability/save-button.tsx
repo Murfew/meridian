9636788ee07a3b0e7b1476c70e7116a4e410f3a1
Murfew <matthew.hurley953@gmail.com>
2026-09-12 18:11:44 -0400

    add save availabilities action

--- components/availability/save-button.tsx
+++ components/availability/save-button.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { useTransition } from "react";
+import { toast } from "sonner";
 import { saveAvailability } from "@/actions/availability";
 import { useAvailability } from "@/components/availability/form";
 import { LoadingButton } from "@/components/loading-button";
-
 export function AvailabilitySaveButton() {
   const { data } = useAvailability();
   const [isPending, startTransition] = useTransition();
@@ -15,7 +15,9 @@ export function AvailabilitySaveButton() {
       loading={isPending}
       onClick={() =>
         startTransition(async () => {
-          await saveAvailability(data);
+          const result = await saveAvailability(data);
+
+          result.ok ? toast.success(result.message) : toast.error(result.error);
         })
       }
     >

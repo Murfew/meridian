177afdf421140a1587a1230e6deea54a9ecd65c2
Murfew <matthew.hurley953@gmail.com>
2026-09-21 13:49:13 -0400

    feat: add inverse time helpers

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -18,12 +18,21 @@ export const DAYS = [
   SATURDAY,
 ];
 
-export function getDayNumber(day: string) {
-  return DAYS.indexOf(day);
+export function getDayNumber(dayName: string) {
+  return DAYS.indexOf(dayName);
+}
+
+export function getDayName(dayNumber: number) {
+  return DAYS.at(dayNumber);
 }
 
 export function toMinutes(time: Time) {
   return time.hours * 60 + time.minutes;
+}
+
+export function fromMinutes(minutes: number): Time {
+  const hours = minutes / 60;
+  return { hours, minutes: minutes - hours * 60 };
 }
 
 export function parseTime(time: string): Time {
@@ -31,4 +40,8 @@ export function parseTime(time: string): Time {
   const minutes = Number(time.slice(3));
 
   return { hours, minutes };
+}
+
+export function formatTime(time: Time): string {
+  return `${time.hours.toString().padStart(2, "0")}:${time.minutes.toString().padStart(2, "0")}`;
 }
